2371c867ea3f1865153a5a2eba1e48038282ebe2
Chikatla Abhiram <abhiramchikatla03@gmail.com>
2025-06-17 14:50:00 +0530

    modified heroSection and added ui components from shadcn

---
+++ components/ui/skeleton.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const Skeleton = ({ className, ...props }: SkeletonProps) => {
+  return (
+    <div
+      className={cn(
+        "animate-pulse rounded-md bg-gray-200/80 dark:bg-gray-700/40",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export { Skeleton };
