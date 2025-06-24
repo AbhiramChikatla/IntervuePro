ddcf4bd03fbfc23917cf0621f4ed6cfab12db6e3
Chikatla Abhiram <abhiramchikatla03@gmail.com>
2025-06-25 03:03:18 +0530

    added types for Feedback & Interview

---
+++ types/index.d.ts
@@ -0,0 +1,26 @@
+export interface Feedback {
+  id: string;
+  interviewId: string;
+  totalScore: number;
+  categoryScores: Array<{
+    name: string;
+    score: number;
+    comment: string;
+  }>;
+  strengths: string[];
+  areasForImprovement: string[];
+  finalAssessment: string;
+  createdAt: string;
+}
+
+export interface Interview {
+  id: string;
+  role: string;
+  level: string;
+  questions: string[];
+  techstack: string[];
+  createdAt: string;
+  userId: string;
+  type: string;
+  finalized: boolean;
+}
