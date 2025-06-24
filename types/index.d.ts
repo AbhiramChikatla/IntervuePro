1b11f36ab07aff4bb450ae19c47db516d726753e
Chikatla Abhiram <abhiramchikatla03@gmail.com>
2025-06-25 03:05:03 +0530

    refactor: formatted the code and added types for Agent and InterviewCard

--- types/index.d.ts
+++ types/index.d.ts
@@ -24,3 +24,36 @@ export interface Interview {
   type: string;
   finalized: boolean;
 }
+
+
+
+export interface CreateFeedbackParams {
+  interviewId: string;
+  userId: string;
+  transcript: { role: string; content: string }[];
+  feedbackId?: string;
+}
+
+export interface User {
+  name: string;
+  email: string;
+  id: string;
+}
+
+export interface InterviewCardProps {
+  interviewId?: string;
+  userId?: string;
+  role: string;
+  type: string;
+  techstack: string[];
+  createdAt?: string;
+}
+
+export interface AgentProps {
+  userName: string;
+  userId?: string;
+  interviewId?: string;
+  feedbackId?: string;
+  type: "generate" | "interview";
+  questions?: string[];
+}
