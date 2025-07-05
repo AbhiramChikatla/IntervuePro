25f1c119986059f64abbf495a5a2336de5b6e9ca
Chikatla Abhiram <abhiramchikatla03@gmail.com>
2025-07-06 02:50:39 +0530

    added some dummy jobs for interview simulation

--- pages/Jobs.tsx
+++ pages/Jobs.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { Card, CardContent } from "@/components/ui/card";
+import { jobs } from "@/data/jobs";
 
 const Jobs = () => {
-  return (
-    <div>
-      Simple Jobs Component
-    </div>
-  )
-}
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <div className="container mx-auto px-4 py-24">
+                <div className="w-full bg-green-50 border-b border-green-200 text-neutral-500 py-3 mb-4">
+                    <div className="container mx-auto px-4">
+                        <p className="text-black-800 text-sm font-medium">
+                            Note :- These are the dummy jobs created by Team
+                            HireMentis for Your Practice.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Jobs
+export default Jobs;
